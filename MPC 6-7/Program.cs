88dbbc51fff8fcb0c0614301dfc9ea09613fbb7b
Jamsson <123484091+Jamsson@users.noreply.github.com>
2023-11-08 09:20:03 +0200

    Add files via upload

---
+++ MPC 6-7/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace MPC_6_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("практика: ");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 6:
+                    Console.Write("Задание: ");
+                    switch (Convert.ToInt32(Console.ReadLine()))
+                    {
+                        case 1:
+                            Random random = new Random();
+                            int rowNumber = random.Next(1, 26);
+
+                            int ticketPrice;
+
+                            if (rowNumber >= 1 && rowNumber <= 3 || rowNumber == 25)
+                            {
+                                ticketPrice = 10;
+                            }
+                            else if (rowNumber >= 4 && rowNumber <= 5)
+                            {
+                                ticketPrice = 15;
+                            }
+                            else if (rowNumber >= 6 && rowNumber <= 24)
+                            {
+                                ticketPrice = 20;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Некорректный номер ряда.");
+                                return;
+                            }
+
+                            Console.WriteLine($"Номер ряда: {rowNumber}");
+                            Console.WriteLine($"Стоимость билета: {ticketPrice} рублей.");
+                            break;
+
+                        case 2:
+                            Console.Write("Введите число n (10<=n<=999): ");
+                            int n = int.Parse(Console.ReadLine());
+
+                            // Находим число х
+                            int x = (n % 10) * 100 + n / 10;
+
+                            Console.WriteLine($"Число x: {x}");
+                            break;
+                    }
+                    break;
+                case 7:
+                    Console.Write("Task:");
+                    switch (Convert.ToInt32(Console.ReadLine()))
+                    {
+                        case 1:
+                            for (int i = 80; i >= 10; i -= 2)
+                                Console.WriteLine(i);
+                            break;
+
+                        case 2:
+                            
+                            break;
+
+                        case 3:
+                            Console.Write("Введите значение b (b >= 100): ");
+                            if (int.TryParse(Console.ReadLine(), out int b) && b >= 100)
+                            {
+                                int sum = 0;
+                                int count = 0;
+
+                                for (int i = 100; i <= b; i++)
+                                {
+                                    sum += i;
+                                    count++;
+                                }
+
+                                double average = (double)sum / count;
+
+                                Console.WriteLine($"Среднее арифметическое всех чисел от 100 до {b}: {average}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Некорректный ввод. Пожалуйста, введите значение b, которое больше или равно 100.");
+                            }
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ PC 6 7/Program.cs
@@ -0,0 +1,172 @@
+﻿namespace PC_6_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Выберите номер практической";
+            Console.Write("Введите номер праклической: ");
+
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 6:
+                    Console.Clear();
+                    Console.Title = "Практическая №6";
+                    Console.Write("Введите номер задачи: ");
+
+                    switch (Convert.ToInt32(Console.ReadLine()))
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.Write("Введите номер маршрута (от 1 до 16): ");
+                            if (int.TryParse(Console.ReadLine(), out int routeNumber) && routeNumber >= 1 && routeNumber <= 16)
+                            {
+                                string bridge;
+
+                                if (routeNumber <= 3 || routeNumber == 5)
+                                {
+                                    bridge = "Южный";
+                                }
+                                else if (routeNumber >= 4 && routeNumber <= 8)
+                                {
+                                    bridge = "Центральный";
+                                }
+                                else if (routeNumber >= 9 && routeNumber <= 12)
+                                {
+                                    bridge = "Северный";
+                                }
+                                else
+                                {
+                                    bridge = "Окружной";
+                                }
+
+                                Console.WriteLine($"Автобус на маршруте {routeNumber} пойдет по {bridge} мосту.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Некорректный ввод. Введите номер маршрута от 1 до 16.");
+                            }
+                            break;
+
+                        case 2:
+                            Console.Clear();
+                            for (int x = 100; x < 1000; x++)
+                            {
+                                int lastDigit = x % 10; // Последняя цифра числа x
+                                int remainingDigits = x / 10; // Оставшиеся две цифры числа x
+                                int reversedRemainingDigits = (remainingDigits % 10) * 10 + remainingDigits / 10; // Переставленные цифры оставшегося числа
+                                int reconstructedNumber = lastDigit * 100 + reversedRemainingDigits; // Восстановленное число
+
+                                if (reconstructedNumber == 654)
+                                {
+                                    Console.WriteLine($"Число х: {x}");
+                                    break; // Прекращаем перебор, если нашли число х
+                                }
+                            }
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Такой задачи нет");
+                            break;
+                    }
+                    break;
+                case 7:
+                    Console.Clear();
+                    Console.Title = "Практическая №7";
+                    Console.Write("Введите номер задачи: ");
+
+                    switch (Convert.ToInt32(Console.ReadLine()))
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.Write("Введите значение A: ");
+                            if (int.TryParse(Console.ReadLine(), out int A))
+                            {
+                                Console.Write("Введите значение B (B >= A): ");
+                                if (int.TryParse(Console.ReadLine(), out int B) && B >= A)
+                                {
+                                    Console.Write("Введите цифру X: ");
+                                    if (int.TryParse(Console.ReadLine(), out int X) && X >= 0 && X <= 9)
+                                    {
+                                        Console.Write("Введите цифру Y: ");
+                                        if (int.TryParse(Console.ReadLine(), out int Y) && Y >= 0 && Y <= 9)
+                                        {
+                                            Console.WriteLine($"Целые числа из диапазона от {A} до {B}, оканчивающиеся на {X} или {Y}:");
+
+                                            for (int i = A; i <= B; i++)
+                                            {
+                                                int lastDigit = Math.Abs(i % 10); // Получаем последнюю цифру числа
+
+                                                if (lastDigit == X || lastDigit == Y)
+                                                {
+                                                    Console.WriteLine(i);
+                                                }
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Некорректный ввод для цифры Y.");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Некорректный ввод для цифры X.");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Некорректный ввод для значения B.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Некорректный ввод для значения A.");
+                            }
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Console.Write("Введите натуральное число n: ");
+                            if (int.TryParse(Console.ReadLine(), out int n) && n > 1)
+                            {
+                                Console.Write($"Разложение числа {n} на простые множители: ");
+                                PrimeFactors(n);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Некорректный ввод. Введите натуральное число больше 1.");
+                            }
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Такой задачи нет");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Такой Практической нет");
+                    break;
+            }
+        }
+        static void PrimeFactors(int num)
+        {
+            int divisor = 2;
+
+            while (num > 1)
+            {
+                if (num % divisor == 0)
+                {
+                    Console.Write(divisor + " ");
+
+                    while (num % divisor == 0)
+                    {
+                        num /= divisor;
+                    }
+                }
+
+                divisor++;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
